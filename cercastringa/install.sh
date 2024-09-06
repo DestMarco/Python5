747eaa996a66a0ce6ab48fd3299fa64a164f1b84
DestMarco <marcolino1424@gmail.com>
2024-09-06 13:52:24 +0200

    ++

---
+++ cercastringa/install.sh
@@ -0,0 +1,5 @@
+mkdir $HOME/cercastringa
+cp cerca.py/cercastringa
+cp requirements.txt/cercastringa
+pip install virtualenv
+pip source myenv/bin/activate
---
+++ myapp/myapp.py
@@ -0,0 +1,8 @@
+import time
+import sys
+t=3
+while True:
+    time.sleep(t)
+    sys.stdout.flush()
+    print("baddragon")
+   

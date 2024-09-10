986e1de84b024b55318b20a889bf9dfe65133f90
DestMarco <marcolino1424@gmail.com>
2024-09-10 14:49:17 +0200

    ++

--- cercastringa/install.sh
+++ cercastringa/install.sh
@@ -1,5 +1,11 @@
 mkdir $HOME/cercastringa
-cp cerca.py/cercastringa
-cp requirements.txt/cercastringa
+cp cerca.py $HOME/cercastringa
+cp requirements.txt $HOME/cercastringa
+cd cercastringa
 pip install virtualenv
 pip source myenv/bin/activate
+
+
+crea la cartella cercastringa con due file dentro (mkdir $HOME/cercastringa)
+copia nella cartella i due file
+crea il virtual enviroment

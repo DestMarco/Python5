0abc00b39876b4fc1219eef1d6ad9736aa7b39bc
DestMarco <marcolino1424@gmail.com>
2024-09-27 15:45:11 +0200

    ++

---
+++ REST/client.py
@@ -0,0 +1,38 @@
+import json, requests, sys
+
+
+base_url="htttp://127.0.0.1:8080"
+
+def RichiedidatiCittadino():
+    nome=input("inserisci nome cittadino")
+    cognome=input("inserisci cognome cittadino")
+    dataNascita=input("inserisci data cittadino")
+    codiceFiscale=input("inserisci codice fiscale cittadino")
+    jReqiuest={"nome":nome, "cognome":cognome, "datanascita":dataNascita, "codicefiscale":codiceFiscale}
+    return jReqiuest
+def CreaInterfaccia ():
+    print("Operazioni disponibbili")
+    print ("1. Inserisci cittadino (es. atto di nascita)")
+    print("2. Richiedi dati di acceso (es.cert. residenza)")
+    print("3. modifica dati cittadino")
+    print("4. ellimina cittadino")
+    print("5. Exit")
+
+
+CreaInterfaccia()
+s0per=input("seleziona operazione")
+while(s0per != "5"):
+    if s0per=="1":
+        api_url=base_url+"/add_cittadino"
+        jsonDataRequest=RichiedidatiCittadino()
+    try:
+        respomse=requests.post(api_url,json=jsonDataRequest)
+        print(respomse.status_code)
+        print(respomse.headers["Contet-Type"])
+        data1=respomse.json()
+        print(data1)
+    except:
+        print("problemi di comunicazione")
+    CreaInterfaccia()
+    s0per=input("seleziona operazione da 1 a 3")
+
---
+++ REST/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, json, request
+
+from myjson import SerializaJson, DeserializeJson
+
+sFileAnagrafe="/.anagrafe.json"
+api=Flask(__name__)
+
+api.route('/add_cittadino', methods=['POST'])
+def GestisciAddCittadino():
+    content_type=request.headers.get('Content-type')
+    print("ricevuta chiamata" + content_type)
+    if content_type=="applicationsJson":
+        jRequest=request.json
+        print(jRequest)
+        jResponse={"Error":"000","Msg":"ok"}
+        return json.dumps(jResponse),200
+    else:
+        return "Errore, formato non riconosciuto",401
+
+api.run(host="127.0.0.1", port=8080)
